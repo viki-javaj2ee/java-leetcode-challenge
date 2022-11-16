6e520480cc331143f8c04016e4982112fa6c2d67
Viki Sangani <viki.javaj2ee@gamil.com>
2022-11-16 09:44:16 +0000

    initial import

---
+++ src/main/java/SymmetricTree_101.java
@@ -0,0 +1,23 @@
+
+public class SymmetricTree_101 {
+
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	public TreeNode(int val) {
+		super();
+		this.val = val;
+	}
+
+	public TreeNode(int val, TreeNode left, TreeNode right) {
+		super();
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+
+}
